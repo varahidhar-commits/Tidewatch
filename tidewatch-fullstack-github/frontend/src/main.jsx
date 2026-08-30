60c0cdfcdeaaaebdfaf0b2fd9e3f8ba318be6944
varahidhar-commits <varahidhar@gmail.com>
2026-08-30 20:18:45 +0530

    Load TideWatch hero enhancements

--- tidewatch-fullstack-github/frontend/src/main.jsx
+++ tidewatch-fullstack-github/frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TideWatchApp from "./App.jsx";
+import "./heroEnhancements.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

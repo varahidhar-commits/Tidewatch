5bd94a73d103f7a1e434e346fa3248b11acb0184
varahidhar-commits <varahidhar@gmail.com>
2026-08-30 20:50:37 +0530

    Enable corrected guided industry demo

--- tidewatch-fullstack-github/frontend/src/main.jsx
+++ tidewatch-fullstack-github/frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import TideWatchApp from "./App.jsx";
 import "./heroLayoutFix.js";
 import "./wholePageI18n.js";
+import "./industryDemoGuide.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

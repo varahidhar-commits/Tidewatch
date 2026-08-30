0bebfb07bfe02b3cf5b37772dee116f2fec5adbd
varahidhar-commits <varahidhar@gmail.com>
2026-08-30 20:26:57 +0530

    Load harbour selector and spacing fixes

--- tidewatch-fullstack-github/frontend/src/main.jsx
+++ tidewatch-fullstack-github/frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import TideWatchApp from "./App.jsx";
 import "./heroEnhancements.js";
+import "./harbourEnhancements.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

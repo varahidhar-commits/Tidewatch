37cc22780dce23221599f80a9d0d5ae76c26d398
varahidhar-commits <varahidhar@gmail.com>
2026-08-30 20:41:50 +0530

    Enable multilingual translation across full landing page

--- tidewatch-fullstack-github/frontend/src/main.jsx
+++ tidewatch-fullstack-github/frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import TideWatchApp from "./App.jsx";
 import "./heroLayoutFix.js";
+import "./wholePageI18n.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
